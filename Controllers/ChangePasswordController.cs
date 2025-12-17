b21c3f3939bcfcf67fe2e48e6b47d01fd15b306c
mryamsh <mryamsh.mohamad@gmail.com>
2025-12-17 16:27:23 +0300

    i guess it is done

--- Controllers/ChangePasswordController.cs
+++ Controllers/ChangePasswordController.cs
@@ -33,8 +33,8 @@ public class AuthController : Microsoft.AspNetCore.Mvc.ControllerBase
             user.OtpCode = otp;
             user.OtpExpiry = DateTime.UtcNow.AddMinutes(5);
             await _db.SaveChangesAsync();
-
-            await _whatsAppService.SendOtp(req.Phone, otp);
+            var to = $"whatsapp:+964{req.Phone.TrimStart('0')}";
+            await _whatsAppService.SendOtp(to, otp);
 
             return Ok(new { message = "OTP sent" });
         }
